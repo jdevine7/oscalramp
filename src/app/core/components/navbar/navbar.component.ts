f6444fe2831c4b823c000b2619b8021d913f9bce
John Devine <johnd@knowledgeservices.com>
2022-12-09 15:32:30 -0500

    more styling, shared module

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +9,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent  {
+  faBars = faBars;
 	isMenuCollapsed = true
 }
