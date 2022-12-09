5791e672d9a1f4b8cbca711d84f8cee725ed207e
John Devine <johnd@knowledgeservices.com>
2022-12-09 11:47:20 -0500

    add multiple catalogs

--- src/app/oscal/services/catalog.service.ts
+++ src/app/oscal/services/catalog.service.ts
@@ -1,18 +1,16 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { ComponentFactoryResolver, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { } from '../../../assets/800-53-rev-5.json'
-import { Catalog, RootCatalog } from '../models/oscal-models';
+import { RootCatalog } from '../models/oscal-models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CatalogService {
-  private url = '../../../assets/800-53-rev-5.json'
-  
   constructor(private http: HttpClient) { }
 
-  getCatalog(): Observable<RootCatalog> {
-    return this.http.get(this.url) as Observable<RootCatalog>;
+  getCatalog(catalogId: string): Observable<RootCatalog> {
+    const fakeJsonUrl = `../../../assets/${catalogId}.json`
+    return this.http.get(fakeJsonUrl) as Observable<RootCatalog>;
   }
 }
