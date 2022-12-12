6d277b4ad26b18e3449d7e6df2855aa6a98e66a4
John Devine <johnd@knowledgeservices.com>
2022-12-12 09:48:58 -0500

    capacitor and android packages

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.oscalramp.app',
+  appName: 'oscalRamp',
+  webDir: 'dist/oscal-ramp',
+  bundledWebRuntime: false
+};
+
+export default config;
